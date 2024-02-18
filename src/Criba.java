c2d7d720648319ce608e42dc9c901e9b2d0c2d42
ROGER-SANCHEZ <rogsanfer@alu.edu.gva.es>
2024-02-18 19:05:23 +0100

    Segundo Commit: Refactorizo Criba.java

--- src/Criba.java
+++ src/Criba.java
@@ -2,45 +2,71 @@ import java.util.Scanner;
 
 public class Criba {
     // Generar números primos de 1 a max
+    public static final int tamanioInicial = 2;
+    public static final boolean verdadero = true;
+    public static final boolean falso = false;
+
+
     public static int[] generarPrimos(int max) {
-        int i;
-        int j;
         if (max >= 2) {
             // Declaraciones
             int dim = max + 1; // Tamaño del array
-            boolean[] esPrimo = new boolean[dim];
-            // Inicializar el array
-            for (i = 0; i < dim; i++)
-                esPrimo[i] = true;
-            // Eliminar el 0  y el 1, que no son primos
-            esPrimo[0] = esPrimo[1] = false;
+            boolean[] esPrimo = inicializarArray(dim);
+
             //Criba
-            for (i = 2; i < Math.sqrt(dim) + 1; i++) {
-                if (esPrimo[i]) {
-                    // Eliminar los múltiplos de i
-                    for (j = 2 * i; j < dim; j += i)
-                        esPrimo[j] = false;
-                }
-            }
+            criba(dim, esPrimo);
+
             // ¿Cuántos primos hay?
-            int cuenta = 0;
-            for (i = 0; i < dim; i++) {
-                if (esPrimo[i])
-                    cuenta++;
-            }
+            int cuenta = cuentaPrimos(dim,esPrimo);
 
             // Rellenar el vector de números primos
-            int[] primos = new int[cuenta];
-            for (i = 0, j = 0; i < dim; i++) {
-                if (esPrimo[i])
-                    primos[j++] = i;
-            }
+            int[] primos = rellenarPrimos(cuenta, dim, esPrimo);
             return primos;
         } else { // max < 2
             return new int[0];
             // vector vacío
         }
     }
+
+    public static boolean [] inicializarArray (int dim){
+        boolean[] esPrimo = new boolean[dim];
+        for (int i = 0; i < dim; i++)
+            esPrimo[i] = verdadero;
+        // Eliminar el 0  y el 1, que no son primos
+        esPrimo[0] = esPrimo[1] = falso;
+        return esPrimo;
+    }
+
+    private static void criba (int dim, boolean[] esPrimo) {
+        for (int i = tamanioInicial; i < Math.sqrt(dim) + 1; i++) {
+            if (esPrimo[i]) {
+                // Eliminar los múltiplos de i
+                for (int j = 2 * i; j < dim; j += i)
+                    esPrimo[j] = false;
+            }
+        }
+    }
+
+    public static int [] rellenarPrimos(int cuenta, int dim, boolean[] esPrimo) {
+        int i;
+        int j;
+        int[] primos = new int[cuenta];
+        for (i = 0, j = 0; i < dim; i++) {
+            if (esPrimo[i])
+                primos[j++] = i;
+        }
+        return primos;
+    }
+
+    private static int cuentaPrimos (int dim, boolean[] esPrimo) {
+        int cuenta = 0;
+        for (int i = 0; i < dim; i++) {
+            if (esPrimo[i])
+                cuenta++;
+        }
+        return cuenta;
+    }
+
     public static void main(String[] args) {
         Scanner teclado=new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
